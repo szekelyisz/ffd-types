2f65b392762cac6579c9cad6bd86b8400d1631fa
Szabolcs Szekelyi <szabi@aura-labs.cc>
2025-03-31 23:55:23 +0200

    Remove FetchError as it's not part of the data model

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,11 +2,7 @@ import { type GeoJSON } from "geojson";
 
 export type TokenId = `0x${string}`;
 
-export type TokenIdOr<T> = TokenId | (T & { _tokenId?: TokenId }) | FetchError;
-
-export interface FetchError {
-  errorMessage: string;
-}
+export type TokenIdOr<T> = TokenId | (T & { _tokenId?: TokenId });
 
 export type Priced<T> = T & { price: Price };
 
